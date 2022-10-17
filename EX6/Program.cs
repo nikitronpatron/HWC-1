46b3364e08b861f7b7b799e3f27c6a96d20260a7
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-17 18:21:42 +0300

    EX6

---
+++ EX6/Program.cs
@@ -0,0 +1,19 @@
+﻿// На ввод: число
+// На вывод: является ли число четным (да/нет)
+
+Console.Write("Enter a number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{number} - is even");
+    Console.ResetColor();
+}
+else 
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{number} - isn`t even");
+    Console.ResetColor();
+}
