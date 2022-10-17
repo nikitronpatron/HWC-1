3ef62aed6d47fb02dfed565a3360290ef3d19ce8
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-17 17:54:45 +0300

    EX4

---
+++ EX4/Program.cs
@@ -0,0 +1,15 @@
+﻿// На вход: три числа
+// На выход: max
+
+Console.WriteLine("Enter three different numbers: ");
+
+int firstnum = Convert.ToInt32(Console.ReadLine());
+int secondnum = Convert.ToInt32(Console.ReadLine());
+int thirdnum = Convert.ToInt32(Console.ReadLine());
+
+int max = firstnum;
+
+if (max < secondnum) max = secondnum;
+if (max < thirdnum) max = thirdnum;
+
+Console.WriteLine($"Max = {max}");
