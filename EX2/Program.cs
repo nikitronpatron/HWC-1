4c27e3de2c825f30676556320ac1092133f0b29c
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-17 17:26:23 +0300

    EX2

---
+++ EX2/Program.cs
@@ -0,0 +1,31 @@
+﻿// На вход: два числа 
+// На выход: max, min
+
+Console.WriteLine("Enter two different numbers: ");
+int firstnum = Convert.ToInt32(Console.ReadLine());
+int secondnum = Convert.ToInt32(Console.ReadLine());
+
+// if (firstnum > secondnum)
+// {
+//     Console.WriteLine($"Max = {firstnum}, min = {secondnum}");
+// } 
+// else(firstnum <= secondnum)
+// {
+//     Console.WriteLine($"Max = {secondnum}, min = {firstnum}");
+// }
+
+int max = 0;
+int min = 0;
+
+if (firstnum > secondnum) 
+{
+    max = firstnum; 
+    min = secondnum;
+}
+if (firstnum <= secondnum) 
+{
+    max = secondnum; 
+    min = firstnum;
+}
+
+Console.WriteLine("Max = " + max + ", min = " + min);
