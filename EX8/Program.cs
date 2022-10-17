cc157e270ba69f787d380c582e241f6e03d92c8d
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-17 18:51:21 +0300

    EX8

---
+++ EX8/Program.cs
@@ -0,0 +1,14 @@
+﻿// На ввод: число (N)
+// На вывод: все четные числа от 1 до (N)
+
+Console.Write("Enter a number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int N = number; N > 1; N--)
+{
+    if (N % 2 == 0)
+    {
+        Console.WriteLine(N);
+    }
+}
